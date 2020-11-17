f717f74596505d4a6c34c1ed89d350ca8a946213
Caio Rodrigo <cacodigo@gmail.com>
2020-11-16 23:54:46 -0300

    Teste

--- select/select.php
+++ select/select.php
@@ -1,16 +1,15 @@
 <?php
 
-require '../../tools/conexao.php';
+// require '../../tools/conexao.php';
 
 // Query para buscar no banco
-$query = "SELECT nome, email, celular, credito, produto, data, hora FROM consorcio_direto WHERE id = 1;";
+// $query = "SELECT nome, email, celular, credito, produto, data, hora FROM consorcio_direto WHERE id = 1;";
 
-$result = mysqli_query($conn, $query);
+// $result = mysqli_query($conn, $query);
 
 // Puxar do banco
-$exe = pg_query($conn, $query);
-$nome = pg_fetch_result($exe, 0, "nome");
-$email = pg_fetch_result($exe, 0, "email");
+// $exe = pg_query($conn, $query);
+// $nome = pg_fetch_result($exe, 0, "nome");
+// $email = pg_fetch_result($exe, 0, "email");
 
 // echo $nome;
-?>
